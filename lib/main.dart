1e11dbbb89a2bbaa9f3f78e8920d84dd5f15e4dd
Eric Oh <77398304+oEriic@users.noreply.github.com>
2021-04-18 13:31:48 -0400

    made task power

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return new MaterialApp(
+      title: 'Remind Me At',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,94 +20,110 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.pink.shade100,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage()
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  createState() => TaskList();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class TaskList extends State<MyHomePage> {
+  List<String> _reminderList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _addTaskItem(String task){
+    if(task.length > 0){
+      setState(() {
+        _reminderList.add(task);
+      });
+    }
+  }
+
+  Widget _buildList(){
+    return new ListView.builder(
+      itemBuilder: (context, index){
+        if(index < _reminderList.length){
+          return _buildReminderItem(_reminderList[index], index);
+        }
+      },
+    );
+  }
+
+  Widget _buildReminderItem(String task, int index){
+    return new ListTile(
+      title: new Text(task),
+      onTap:() => _askRemoveTask(index)
+    );
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget build(BuildContext context){
+    return new Scaffold(appBar: new AppBar(
+      title: Text('Reminders')
+    ),
+    body: _buildList(),
+    floatingActionButton: new FloatingActionButton(
+      onPressed: _pushAddTaskScreen,
+      tooltip: 'Add Tasks',
+      child: new Icon(Icons.add),
+    ),
     );
+  }
+
+  void _pushAddTaskScreen(){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context){
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Add Task')
+            ),
+            body: TextField(
+              autofocus: true,
+              onSubmitted: (val){
+                _addTaskItem(val);
+                Navigator.pop(context);
+            },
+            decoration: InputDecoration(
+              hintText: 'Enter a task to be reminded',
+              contentPadding: const EdgeInsets.all(20.0)
+            ),
+            )
+          );
+      }
+      )
+    );
+  }
+
+  void _removeTaskItem(int index){
+    setState(() => _reminderList.removeAt(index));
+  }
+
+  void _askRemoveTask(int index){
+    showDialog(
+      context: context, 
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Remove "${_reminderList[index]}"?'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop()
+              ),
+            FlatButton( 
+              child: Text('Mark as Complete'),
+              onPressed: (){
+                _removeTaskItem(index);
+                Navigator.of(context).pop();
+              }
+              )
+          ]
+        );
+      }
+      );
   }
 }
